45c1ad6928cfa82058ecad2a6184026f14b9727f
idemudia2002 <idde19@yahoo.com>
2020-11-05 10:59:10 +0100

    Changed color

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,13 @@ int RED = 3;
 
 int DELAY_GREEN = 5000;
 int DELAY_YELLOW = 2000;
-int DELAY_RED = 5000;
+int DELAY_RED = 2000;
+
+void yellow_light() {
+  digitalWrite(GREEN, LOW);
+  digitalWrite(YELLOW, HIGH);
+  digitalWrite(RED, LOW);
+}
 
 void setup() {
  pinMode(RED, OUTPUT);
@@ -15,29 +21,26 @@ void setup() {
 }
 
 
-void green_light() {
-  digitalWrite(GREEN, HIGH);
-  digitalWrite(YELLOW, LOW);
-  digitalWrite(RED, LOW);
-}
-
 void red_light() {
   digitalWrite(GREEN, LOW);
   digitalWrite(YELLOW, LOW);
   digitalWrite(RED, HIGH);
 }
 
-void yellow_light() {
-  digitalWrite(GREEN, LOW);
-  digitalWrite(YELLOW, HIGH);
+
+
+void green_light() {
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(YELLOW, LOW);
   digitalWrite(RED, LOW);
 }
 
+
 void loop() {
- green_light();
- delay(DELAY_GREEN);
  yellow_light();
  delay(DELAY_YELLOW);
  red_light();
  delay(DELAY_RED);
+ green_light();
+ delay(DELAY_GREEN);
 }
